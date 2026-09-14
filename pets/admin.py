b08a7a317b1e1d11a58d1c9ae7c0d57a7c88b9b2
Таня <berezov.tatyana@gmail.com>
2026-09-14 21:40:11 +0700

    14.09.2026/2

--- pets/admin.py
+++ pets/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Pet
 
-admin.site.register(Pet)
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'breed', 'owner', 'is_public')
+    list_filter = ('is_public', 'breed')
+    search_fields = ('name', 'breed', 'owner__username')
+    prepopulated_fields = {'slug': ('breed', 'name')}
+    list_editable = ('is_public',)
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('owner', 'name', 'breed', 'slug')
+        }),
+        ('Описание и доступ', {
+            'fields': ('bio', 'is_public')
+        }),
+    )
 
--- pets/views.py
+++ pets/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import (
                             DetailView, CreateView,
-                            UpdateView, TemplateView
+                            UpdateView, TemplateView, 
+                            DeleteView
                         )
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
@@ -37,6 +39,22 @@ class PetUpdateView(LoginRequiredMixin, PetOwnerRequiredMixin, PageTitleMixin, U
     context_object_name = Pet
     page_title = 'Редактировать питомца'
     
+
+class PetDeleteView(LoginRequiredMixin, PetOwnerRequiredMixin, PageTitleMixin, DeleteView):
+    model = Pet
+    template_name = 'pets/pet_confirm_delete.html'
+    context_object_name = Pet
+    page_title = 'Удалить питомца'
+    success_url = reverse_lazy('pets:pet_list')
+
+class PetListOwnerView(LoginRequiredMixin, PageTitleMixin, TemplateView):
+    template_name = 'pets/pet_list.html'
+    page_title = 'Мои питомцы'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pets'] = Pet.objects.filter(owner=self.request.user)
+        return context
 
 @method_decorator(cache_page(60*10), name='dispatch')
 class VetClinicDirectoryView(PageTitleMixin, TemplateView):
